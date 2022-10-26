5daf8b6a3557e308dd32b498960c6f9d0adb60fe
Paul Cameron <pcameron5@gmail.com>
2022-10-26 10:56:38 -0400

    CMOD-10262022: Home and About Page creation

--- outlet-practice/src/App.js
+++ outlet-practice/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5}}>
+          Home
+        </Link>
+        <Link to="/about" style={{ padding: 5 }}>
+          About
+        </Link>
+      </nav>
+      <Routes>
+        <Route path ="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
---
+++ outlet-practice/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Home View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+      <h4>
+        <a href="https://blog.logrocket.com/react-router-v6/">
+          Tutorial from logRocket
+        </a>
+      </h4>
+    </div>
+  );
+}
+
+export default Home;
